8d37103058f644b1f7ae97937f2b60e40d068e76
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2024-05-02 18:12:13 +0200

    added more alpha params for hyperparam search

--- ny_taxi/modeling/model.py
+++ ny_taxi/modeling/model.py
@@ -15,7 +15,7 @@ def get_regressor(
         regressor_params = {"regressor__fit_intercept": [True, False]}
     elif regressor_type == "ridge":
         regressor = Ridge(random_state=41)
-        regressor_params = {"regressor__alpha": np.array([0.1, 0.5, 1, 3])}
+        regressor_params = {"regressor__alpha": np.array([0.05, 0.1, 0.5, 1, 3, 5])}
     else:
         logging.info(f"unidentified regressor_type: {regressor_type}")
     return regressor, regressor_params
